ceed0cafe647059ab9b50fcaa267005bc8ed127c
aidansneal <an820715@ohio.edu>
2020-02-17 14:42:40 -0500

    Unity files

---
+++ 3401_Week05_Neal/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public AudioSource buttonAudioSource1;
+
+    public void OnButtonClicked()
+    {
+        if (buttonAudioSource1.isPlaying)
+        {
+            buttonAudioSource1.Stop();
+        }
+        else
+        {
+            buttonAudioSource1.Play();
+        }
+    }
+
+
+
+}
